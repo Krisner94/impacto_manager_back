af6f9b072b6dd73e3ea05f684b53f47349cde441
rhama <rhama.krisner@gmail.com>
2025-05-04 10:11:46 -0300

    fix: lose user repository

--- src/main/java/application/impacto_manager_back/model/Aluno.java
+++ src/main/java/application/impacto_manager_back/model/Aluno.java
@@ -30,35 +30,35 @@ import java.util.List;
 @Entity
 @Table(name = "aluno")
 public class Aluno implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    private String cpf;
-    private String sexo;
-    
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private String dataNascimento;
-    private String telefone;
-    
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "endereco_id")
-    private Endereco endereco;
-    
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-      name = "aluno_responsavel",
-      joinColumns = @JoinColumn(name = "aluno_id"),
-      inverseJoinColumns = @JoinColumn(name = "responsavel_id")
-    )
-    private List<Responsavel> responsavel = new ArrayList<>();
-    
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-      name = "aluno_turma",
-      joinColumns = @JoinColumn(name = "aluno_id"),
-      inverseJoinColumns = @JoinColumn(name = "turma_id")
-    )
-    @JsonIgnoreProperties({"alunos", "professores"})
-    private List<Turma> turmas = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String sexo;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private String dataNascimento;
+	private String telefone;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "endereco_id")
+	private Endereco endereco;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinTable(
+		name = "aluno_responsavel",
+		joinColumns = @JoinColumn(name = "aluno_id"),
+		inverseJoinColumns = @JoinColumn(name = "responsavel_id")
+	)
+	private List<Responsavel> responsavel = new ArrayList<>();
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+		name = "aluno_turma",
+		joinColumns = @JoinColumn(name = "aluno_id"),
+		inverseJoinColumns = @JoinColumn(name = "turma_id")
+	)
+	@JsonIgnoreProperties({"alunos", "professores"})
+	private List<Turma> turmas = new ArrayList<>();
 }
